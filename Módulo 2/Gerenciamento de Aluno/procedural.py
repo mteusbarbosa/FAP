240282e09172b96b9d06daea7aec2b77245da5ce
Mateus Barbosa <mateus.bm13@gmail.com>
2024-07-19 09:21:59 -0300

    Feature: Menu principal do programa

--- Módulo 2/Gerenciamento de Aluno/procedural.py
+++ Módulo 2/Gerenciamento de Aluno/procedural.py
@@ -23,3 +23,55 @@ Para ambos os exercícios:
 Bônus: Implemente a funcionalidade de salvar e carregar os dados dos alunos 
 em um arquivo, tanto na versão procedural quanto na orientada a objetos.
 '''
+
+while True:
+  print("[1] Cadastrar um novo aluno")
+  print("[2] Listar todos os alunos cadastrados")
+  print("[3] Editar informações de um aluno existente")
+  print("[4] Excluir um aluno do sistema")
+  print("[5] Calcular e exibir a média das notas de cada aluno")
+  print("[6] Carregar dados de arquivo")
+  print("[7] Salvar dados em arquivo")
+  print("[0] Sair")
+
+  try:
+    opcao = int(input("Escolha uma opção: "))
+
+    if opcao < 0 or opcao > 7:
+      print("-\nOpção inválida. Tente novamente.\n-")
+      continue
+
+    if opcao == 1:
+      #cadastrar_novo_aluno()
+      continue
+  
+    if opcao == 2:
+      #listar_alunos_cadastrados()
+      continue
+  
+    if opcao == 3:
+      #editar_aluno_existente()
+      continue
+  
+    if opcao == 4:
+      #excluir_aluno()
+      continue
+  
+    if opcao == 5:
+      #exibir_media_alunos()
+      continue
+  
+    if opcao == 6:
+      #carregar_dados_arquivo()
+      continue
+
+    if opcao == 7:
+      #salvar_alunos_arquivo()
+      continue
+
+    if opcao == 0:
+      print("-\nSaindo do programa\n-")
+      break
+
+  except ValueError:
+    print("-\nOpção inválida. Tente novamente.\n-")
